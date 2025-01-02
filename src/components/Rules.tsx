51f653efc7743962612419e57a5271e5621ff885
DrBiznes <jamesfemino@gmail.com>
2025-01-02 01:29:04 -0800

    updated rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,28 +1,24 @@
 import '../App.css';
+import { Separator } from './ui/separator';
 
 export function Rules() {
   return (
-    <div className="text-lg pt-6 font-comic">
-      <h2 className="flex items-center gap-2 text-2xl font-bold mb-4 font-comic">
+    <div className="text-lg pt-4 font-comic">
+      <h2 className="flex items-center justify-center gap-2 text-2xl font-bold mb-2 font-comic">
         <span className="material-icons">gavel</span>
         Rules
       </h2>
-      <ol>
-        <li>Name as many women as you can</li>
-        <li>Each name must be unique</li>
-        <li>Names are verified through Wikipedia</li>
-        <li>You have unlimited time</li>
-        <li>Goal is to reach 100 names</li>
-        <li>Faster times get higher rankings</li>
+      <div className="flex justify-center">
+        <Separator className="my-2 w-2/3" />
+      </div>
+      <ol className="list-decimal pl-6 font-bold">
+        <li className="mb-2">DONT USE GOOGLE</li>
+        <li className="mb-2">CAN BE DEAD WOMEN</li>
+        <li className="mb-2">THEY MUST HAVE A WIKIPEDIA PAGE</li>
+        <li className="mb-2">no fictional characters must be real ass women or a god</li>
+        <li className="mb-2">Capitalization and accent marks don't matter</li>
+        <li className="mb-2">Press ENTER or TAB to verify each name and automatically move the cursor to the next box</li>
       </ol>
-      
-      <h3 className="text-xl font-bold mt-6 mb-4 font-comic">Tips:</h3>
-      <ul>
-        <li>Think of historical figures</li>
-        <li>Include actresses and artists</li>
-        <li>Remember political leaders</li>
-        <li>Consider scientists and inventors</li>
-      </ul>
     </div>
   );
 } 
